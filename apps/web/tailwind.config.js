261b05a92361681f04408ee9ed7c58af8d716b27
Aron <blazte.aster@gmail.com>
2024-05-20 18:04:38 -0500

    chore(apps/web): Setup tailwind extending global tailwind.config

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+const sharedConfig = require("@repo/tailwind-config/tailwind.config.js");
+module.exports = {
+    ...sharedConfig,
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+};
