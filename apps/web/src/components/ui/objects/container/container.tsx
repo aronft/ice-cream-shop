cb809ccaff42500a19fcd5a2779a5372abae79ff
Aron <blazte.aster@gmail.com>
2024-05-21 17:47:07 -0500

    feat(apps/web): Add container component

---
+++ apps/web/src/components/ui/objects/container/container.tsx
@@ -0,0 +1,15 @@
+import { HTMLAttributes } from "react";
+import { cn } from "../../../../lib/utils/class-name";
+
+interface ContainerProps extends HTMLAttributes<{}> {
+    tag?: keyof Pick<JSX.IntrinsicElements, "section" | "div" | "main" | "article">;
+}
+
+export const Container = ({ tag = "div", children, className, ...props }: ContainerProps) => {
+    const Tag = tag;
+    return (
+        <Tag className={cn("container px-8 mx-auto", className)} {...props}>
+            {children}
+        </Tag>
+    );
+};
