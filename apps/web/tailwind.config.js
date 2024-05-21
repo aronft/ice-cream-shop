1498e88acbe6599ef5e66e0e29bd7386e33aaf6f
Aron <blazte.aster@gmail.com>
2024-05-21 15:32:47 -0500

    chore(apps/web): Add tailwind colors to web

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -13,6 +13,19 @@ module.exports = {
     ],
 
     theme: {
+        colors: {
+            primary: {
+                50: "#f2f9fd",
+                950: "#102f41",
+            },
+            secondary: {
+                600: "#ca2e55",
+            },
+            tertiary: {
+                600: "#8a6552",
+            },
+            transparent: "transparent",
+        },
         extend: {
             fontFamily: {
                 sans: ["var(--secondary-font)", "sans-serif"],
