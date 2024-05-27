3f891428eaf886450b6ee96881ec5e0ed60911b3
Aron <blazte.aster@gmail.com>
2024-05-27 16:32:36 -0500

    feat(apps/web): add marquee section

---
+++ apps/web/src/components/sections/seporator-section/separator-section.tsx
@@ -0,0 +1,17 @@
+import Marquee from "react-fast-marquee";
+import { FaIceCream } from "react-icons/fa";
+
+export const SeparatorSection = () => {
+    return (
+        <section className="bg-secondary-600 py-10 rounded-tl-3xl rounded-br-3xl">
+            <div className="bg-white py-0.5  -rotate-[3deg]">
+                <Marquee className="" autoFill speed={250}>
+                    <span className="uppercase inline-flex mx-4 text-5xl font-medium  text-primary-950 gap-2 ">
+                        <FaIceCream className="text-primary-500" />
+                        Buy yours now
+                    </span>
+                </Marquee>
+            </div>
+        </section>
+    );
+};
