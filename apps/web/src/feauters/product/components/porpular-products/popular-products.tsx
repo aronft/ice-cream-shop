b9f802b50e213ebd3ac97dbde128cf503e19881f
Aron <blazte.aster@gmail.com>
2024-05-28 11:41:22 -0500

    feat(apps/web): add product cart

---
+++ apps/web/src/feauters/product/components/porpular-products/popular-products.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/ui/objects/container/container";
+
+export const PopularProducts = () => {
+    return (
+        <section className="py-16">
+            <Container>
+                <h2>Popular Products</h2>
+            </Container>
+        </section>
+    );
+};
---
+++ apps/web/src/feauters/product/components/product-card/product-card.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { ProductModel } from "../../models/product.model";
+import { AiOutlineShopping } from "react-icons/ai";
+import Link from "next/link";
+
+interface ProductCardProps extends ProductModel {
+    titleTag: keyof Pick<JSX.IntrinsicElements, "h2" | "h3" | "h4">;
+}
+
+export const ProductCard = ({ src, name, price, rating, titleTag }: ProductCardProps) => {
+    const TitleTag = titleTag;
+    return (
+        <Link href={`/product/${name}`}>
+            <article className="flex flex-col gap-4 p-2  rounded-xl">
+                <header className="pt-10 overflow-hidden ">
+                    <div className="bg-primary-100  rounded-3xl">
+                        <Image
+                            src={src}
+                            alt={name}
+                            width={270}
+                            height={190}
+                            className="w-full h-48 object-contain scale-125 right-0   rotate-[25deg] object-top"
+                        />
+                    </div>
+                </header>
+                <footer className="flex justify-between items-start">
+                    <div className="flex flex-wrap">
+                        <TitleTag className="w-full font-bold text-tertiary-950">{name}</TitleTag>
+                        <span className="text-tertiary-950 font-medium">${price}</span>
+                        <span>{rating}</span>
+                    </div>
+                    <button className="border group border-tertiary-100 rounded-full p-2 hover:bg-tertiary-900">
+                        <AiOutlineShopping className="text-2xl group-hover:text-white" />
+                    </button>
+                </footer>
+            </article>
+        </Link>
+    );
+};
