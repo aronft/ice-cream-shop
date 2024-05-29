f93019f4906e8ae967e36160addff97205a35b5e
Aron <blazte.aster@gmail.com>
2024-05-29 15:43:15 -0500

    refactor(apps/web): move type to models folders

--- apps/web/src/components/illustrations/bg-strawbarry.tsx
+++ apps/web/src/components/illustrations/bg-strawbarry.tsx
@@ -1,4 +1,4 @@
-export type SVGProps = React.SVGAttributes<SVGElement>;
+import { SVGProps } from "@/lib/models/svg.model";
 
 export const BgStrawbarry = ({ ...props }: SVGProps) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" {...props}>
