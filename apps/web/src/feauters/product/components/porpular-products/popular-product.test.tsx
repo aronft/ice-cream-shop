5fffce1dafa9c8507b30f43774c57e1220a1e1a0
Aron <blazte.aster@gmail.com>
2024-05-29 14:29:36 -0500

    fix(Root): add babel support to render components (#9)

---
+++ apps/web/src/feauters/product/components/porpular-products/popular-product.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { PopularProducts } from "./popular-products";
+
+describe("Test Product Card ", () => {
+    it("", () => {
+        render(<PopularProducts />);
+    });
+});
