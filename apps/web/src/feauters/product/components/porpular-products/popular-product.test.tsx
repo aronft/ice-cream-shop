7b226586911290973e1565355ae221f27156127f
Aron <blazte.aster@gmail.com>
2024-05-30 12:14:28 -0500

    test(apps/web): popular product suction show product

--- apps/web/src/feauters/product/components/porpular-products/popular-product.test.tsx
+++ apps/web/src/feauters/product/components/porpular-products/popular-product.test.tsx
@@ -1,8 +1,21 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { PopularProducts } from "./popular-products";
+import { ProductModel } from "../../models/product.model";
+import image from "@/assets/ice-cream-1.png";
+
+const product: ProductModel = {
+    id: "1",
+    src: image.src,
+    name: "Ice Cream",
+    price: 10,
+    rating: 4,
+};
 
 describe("Test Product Card ", () => {
-    it("", () => {
-        render(<PopularProducts />);
+    it("should list courses", async () => {
+        render(<PopularProducts products={[product]} />);
+
+        const productLink = await screen.findByRole("link", { name: `Product ${product.name}` });
+        expect(productLink).toBeInTheDocument();
     });
 });
--- apps/web/test-setup.ts
+++ apps/web/test-setup.ts
@@ -1,4 +1,5 @@
 import "@testing-library/jest-dom";
+import crypto from "crypto";
 // import "@testing-library/jest-dom/extend-expect";
 // import "regenerator-runtime/runtime";
 
