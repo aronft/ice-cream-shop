9f01be5465e996047803f958f2ceed5b0356a192
Aron <blazte.aster@gmail.com>
2024-06-04 10:25:08 -0500

    feat(apps/web): add masterful flavors section and import into home page

--- apps/web/src/app/(shop)/page.tsx
+++ apps/web/src/app/(shop)/page.tsx
@@ -1,4 +1,5 @@
 import { HeroSection } from "@/components/sections/hero-section/hero-section";
+import MasterfulFlavorsSection from "@/components/sections/masterful-flavors-section/masterful-flavors-section";
 import { SeparatorSection } from "@/components/sections/seporator-section/separator-section";
 import { PopularProducts } from "@/feauters/product/components/porpular-products/popular-products";
 import { getPopularProducts } from "@/feauters/services/get-popular-products";
@@ -10,6 +11,7 @@ export default async function Page() {
             <HeroSection />
             <PopularProducts products={products} />
             <SeparatorSection />
+            <MasterfulFlavorsSection />
         </div>
     );
 }
