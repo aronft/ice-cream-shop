dae9eb422237bbf12d5b4ebac4211eac86b2cf4e
Aron <blazte.aster@gmail.com>
2024-06-04 10:26:13 -0500

    Artisan home section (#15)
    * chore(apps/web): add image resource for artisan section
    
    * feat(apps/web): add masterful flavors section and import into home page

---
+++ apps/web/src/components/sections/masterful-flavors-section/masterful-flavors-section.tsx
@@ -0,0 +1,36 @@
+import { Container } from "@/components/ui/objects/container/container";
+import Image from "next/image";
+import masterFulImage from "@/assets/dropping-ice-cream.png";
+
+const MasterfulFlavorsSection = () => {
+    return (
+        <section className="">
+            <Container className="flex relative py-16 flex-col column-gap-8 items-center md:flex-row-reverse md:justify-between">
+                <Image
+                    width={720}
+                    height={504}
+                    src={masterFulImage}
+                    alt="dropping honey over the icecream"
+                    className="aspect-[4:3] w-[50vw] max-w-2xl absolute bottom-0 md:h-full top-0 object-cover"
+                />
+                <Image
+                    width={720}
+                    height={504}
+                    src={masterFulImage}
+                    alt="dropping honey over the icecream"
+                    className="aspect-[4:3] w-[50vw] max-w-2xl object-cover invisible"
+                />
+                <div className="text-center flex flex-col gap-4 md:text-start   max-w-xl ">
+                    <h2 className="text-4xl font-bold">Masterful Flavors</h2>
+                    <p className="md:text-lg">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id
+                        penatibus imperdiet. Turpis egestas ultricies purus auctor tincidunt lacus
+                        nunc.
+                    </p>
+                </div>
+            </Container>
+        </section>
+    );
+};
+
+export default MasterfulFlavorsSection;
